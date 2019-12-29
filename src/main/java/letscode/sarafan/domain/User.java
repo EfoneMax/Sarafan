9ce277118a151bb4542c6e9c0963ebdd2fac6ca1
glushkov <glushkov@proitr.ru>
2019-12-29 19:41:44 +0500

    7

--- src/main/java/letscode/sarafan/domain/User.java
+++ src/main/java/letscode/sarafan/domain/User.java
@@ -1,15 +1,17 @@
 package letscode.sarafan.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "usr")
-public class User {
+public class User implements Serializable {
     public String getId() {
         return id;
     }
@@ -73,5 +75,6 @@ public class User {
     private String email;
     private String gender;
     private String locale;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime lastVisit;
 }
